538fb0e460629029397f43159ac2f8769844fa19
Pranay Waghmare <waghmareps12@gmail.com>
2025-02-14 17:43:24 -0500

    changes

--- src/train.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from transformers import AutoTokenizer
-from datasets import load_dataset
-from model import SmallLanguageModel, ModelConfig  # Remove src. prefix
-import random
-# ... rest of the code ... 
